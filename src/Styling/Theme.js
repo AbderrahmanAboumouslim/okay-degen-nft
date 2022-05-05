e485fa1025d13d2302d0cc17c47cc174f13be0b5
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-05-05 15:33:51 +0000

    light and dark object, install Inter and R R DOM

---
+++ src/Styling/Theme.js
@@ -0,0 +1,43 @@
+export const light = {
+  body: 'rgb(6, 17, 60)',
+  text: 'rgb(255, 140, 50)',
+
+  bodyRgba: '255, 255, 255',
+  textRgba: '221, 221, 221',
+
+  carouselColor: '#EEEDDE',
+
+  fontxs: '0.75em',
+  fontsm: '0.875em',
+  fontmd: '1em',
+  fontlg: '1.25em',
+  fontxl: '2em',
+  fontxxl: '3em',
+  fontxxxl: '4em',
+
+  fontButton: '0.875em',
+
+  navHeight: '5rem',
+};
+
+export const dark = {
+  text: 'rgb(6, 17, 60)',
+  body: 'rgb(255, 140, 50)',
+
+  textRgba: '255, 255, 255',
+  bodyRgba: '221, 221, 221',
+
+  carouselColor: '#EEEDDE',
+
+  fontxs: '0.75em',
+  fontsm: '0.875em',
+  fontmd: '1em',
+  fontlg: '1.25em',
+  fontxl: '2em',
+  fontxxl: '3em',
+  fontxxxl: '4em',
+
+  fontButton: '0.875em',
+
+  navHeight: '5rem',
+};
