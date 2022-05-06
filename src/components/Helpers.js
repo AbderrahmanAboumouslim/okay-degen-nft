78a5d39c6f4d7028303f739503e9ade15cc1934b
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-05-06 18:33:35 +0000

    Hard part of roadmap is done

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -1,1 +1,34 @@
 export const linkToMint = 'https://www.google.com';
+
+export const phases = {
+  phase1: [
+    '- Create a collection of 555 okay degens',
+    '- Set up discord and twitter',
+    '- Give out WL and OG',
+    '- Determine a mint date',
+    '- END OF MINT',
+  ],
+  phase2: [
+    '- Get lissted on  ME',
+    '- Set up Holder Verification',
+    '- Add 500 SOL into LP for staking token $OKDEGEN',
+    '- Come out with Staking System',
+    '- Raffle NFTs ussing token $OKDEGEN',
+  ],
+  phase3: [
+    '- Release our own launchpad and doxing service to ensure a rug free ecosystem.',
+    '- Open up applications for projects to be on our launchpad.',
+    '- Holders will receive 50% of revenue made from launchpad (We will take 6% of the mint monney of the projects which will launch on the Okaypad).',
+    '- Only staked Okaydegen NFTs receive revenue.',
+  ],
+  phase4: [
+    '- Send your Okaydegen on a mission to find a pack and go hunt for $OKDEGEN, the hunt last 20 days and you will receive 1.7x more $OKDEGEN (Price to set your Okaydegen on a mission will be revealed once token is made).',
+  ],
+  phase5: [
+    '- There will be 10 1/1 Legendary OkayDegens, these degens will receive 5x $OKDEGEN coin from normal staking, the auctions will be held in SOL (75% of ALL auctions will go to the community wallet, Votes will be held for what the money is used for in the wallet).',
+  ],
+  phase6: [
+    '- Gather enough $OKDEGEN to mint a female Okaydegen, Female okay degens will earn an additional 1.5x $OKDEGEN from the normal staking. (females will not be able to form a pack and hunt).',
+    '- Female okay degens will also be listed on ME.',
+  ],
+};
--- src/components/pages/Roadmap.js
+++ src/components/pages/Roadmap.js
@@ -1,8 +1,119 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { phases } from '../Helpers';
 
 const Roadmap = () => {
-  return <h1>Roadmap page|</h1>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [show, setShow] = useState([]);
+
+  const LeftDisplay = id => {
+    setIsOpen(!isOpen);
+    setShow(id);
+  };
+
+  return (
+    <Wrapper>
+      <Grid>
+        <Left onClick={() => LeftDisplay(phases.phase1)}>PHASE 1</Left>
+        <Center>
+          <One onClick={() => LeftDisplay(phases.phase2)}>PHASE 2</One>
+          <Two onClick={() => LeftDisplay(phases.phase3)}>PHASE 3</Two>
+          <Three onClick={() => LeftDisplay(phases.phase4)}>PHASE 4</Three>
+          <Four onClick={() => LeftDisplay(phases.phase5)}>PHASE 5</Four>
+        </Center>
+        <Right onClick={() => LeftDisplay(phases.phase6)}>PHASE 6</Right>
+
+        {isOpen ? (
+          <div className="ai">
+            <h1 onClick={() => setIsOpen(!isOpen)}>Go back to roadmap</h1>
+            {show.map((s, i) => {
+              return <div key={i}>{s}</div>;
+            })}
+          </div>
+        ) : null}
+      </Grid>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: ${props => props.theme.dColor};
+  color: ${props => props.theme.bColor};
+`;
+
+const Grid = styled.div`
+  cursor: pointer;
+  display: grid;
+  grid-template-areas: 'left center center right' 'left center center right';
+  gap: 1rem;
+
+  width: 80vw;
+  height: 70vh;
+  margin: 0 auto;
+
+  position: relative;
+
+  .ai {
+    position: absolute;
+    inset: 0;
+    background-color: black;
+    opacity: 0.8;
+  }
+`;
+const Left = styled.div`
+  grid-area: left;
+  background-color: gray;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Center = styled.div`
+  grid-area: center;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+`;
+const Right = styled.div`
+  grid-area: right;
+  background-color: aqua;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const One = styled.div`
+  background-color: lightblue;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Two = styled.div`
+  background-color: lightcoral;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Three = styled.div`
+  background-color: lightsalmon;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Four = styled.div`
+  background-color: lightgray;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Roadmap;
